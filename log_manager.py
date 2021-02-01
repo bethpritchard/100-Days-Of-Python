c9c2332619e56caa7c0fbbd8534d0a3ee962c9e9
bethpritchard <60654572+bethpritchard@users.noreply.github.com>
2021-02-01 16:15:04 +0000

    initial commit

---
+++ log_manager.py
@@ -0,0 +1,41 @@
+from datetime import date, timedelta
+import os,sys
+import os.path
+from os import path
+
+START_DAY = 31
+end_day = START_DAY + 10
+
+
+if path.exists(f'log/days{START_DAY}-{end_day - 1}.md'):
+    print("File already exists.")
+
+else:
+    with open(f"log/days{START_DAY}-{end_day - 1}.txt", "a") as new_log:
+        for i in range(10):
+            d = date.today() + timedelta(days=i)
+            d2 = d.strftime("%d %B %Y")
+            new_day = f"""### Day {START_DAY + i}: {d2}
+    **Today's progress:**
+    
+    **Today I've learned about:**
+    
+    **Thoughts:**
+    
+    **Link to work:**
+    \n
+    """
+            new_log.write(new_day)
+
+# ----------- Change to md file ------------
+folder = 'log'
+try:
+
+    for filename in os.listdir(folder):
+           infilename = os.path.join(folder,filename)
+           if not os.path.isfile(infilename): continue
+           oldbase = os.path.splitext(filename)
+           newname = infilename.replace('.txt', '.md')
+           output = os.rename(infilename, newname)
+except FileExistsError:
+    print("File already exists")
